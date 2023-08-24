99fa1069cb211c13138e04bbb5d6a0a0bc551c7f
codeSparrow092 <140591560+codeSparrow092@users.noreply.github.com>
2023-08-24 19:43:14 +0530

    2 files modified

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-// Import stylesheets
-import './style.css';
+const randomColor = function () {
+  const hex = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color = color + hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+let setinterval;
+const startChangingColor = function () {
+  changeColor = function () {
+    document.body.style.backgroundColor = randomColor();
+  };
+  if (!setinterval) {
+    setinterval = setInterval(changeColor, 1000);
+  }
+};
+const stopChangingColor = function () {
+  clearInterval(setinterval);
+  setinterval = null;
+};
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+document.querySelector('#start').addEventListener('click', startChangingColor);
+document.querySelector('#stop').addEventListener('click', stopChangingColor);
